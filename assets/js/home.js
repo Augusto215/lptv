ab871f5024e35bcacd1c57a5ad332d45bc51cc39
Augusto215 <augusto.dataanalysis@gmail.com>
2023-07-30 17:19:34 -0300

    second commit

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,3 +4,28 @@
     video.addEventListener('loadedmetadata', function() {
         this.currentTime = 60; // O tempo é definido em segundos, então 60 segundos = 1 minuto
     }, false);
+
+
+    document.querySelectorAll('.contrate-a').forEach(function(el) {
+        el.setAttribute('href', 'https://api.whatsapp.com/send?phone=5555199832779&text=Ol%C3%A1!%20Vim%20pelo%20site%20e%20gostaria%20de%20saber%20mais%20sobre%20a%20IPTV!');
+    });
+
+// Selecionando todos os botões
+const items = document.querySelectorAll('.accordion button');
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+
+
